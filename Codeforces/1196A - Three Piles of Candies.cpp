b9862cfedfe94f533aa3f98cace9fc8a6f57aca7
ahmedfahad04 <60494055+ahmedfahad04@users.noreply.github.com>
2020-10-30 22:35:00 +0600

    Add files via upload

---
+++ Codeforces/1196A - Three Piles of Candies.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define N 1000007
+#define pb push_back
+using namespace std;
+
+int main()
+{
+	ll q;
+	cin >> q;
+	
+	while(q--)
+	{
+		ll a,b,c;
+		cin >> a >> b >> c;
+		cout << (a+b+c)/2 << endl;
+	}
+}
+
+
